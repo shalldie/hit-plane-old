313abda5d6cd61206b4a35468f26d0c918000d8c
谢爽 <394229785@qq.com>
2016-10-30 21:34:22 +0800

    调整目录结构

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var child = require('child_process');
 var path = require('path');
 
-child.exec(`explorer ${path.join(__dirname, 'test', 'index.html')}`);
+child.exec(`explorer ${path.join(__dirname, 'index.html')}`);
 
-child.exec(`open ${path.join(__dirname, 'test', 'index.html')}`);
+child.exec(`open ${path.join(__dirname, 'index.html')}`);
