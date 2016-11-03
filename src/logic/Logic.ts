885b04b9101536f2c206fd5ecf91df2f995f6d54
谢爽 <394229785@qq.com>
2016-11-03 16:08:23 +0800

    修改敌军出现速度

--- src/logic/Logic.ts
+++ src/logic/Logic.ts
@@ -64,7 +64,7 @@ export default class Logic {
         this.newPlane();
         setInterval(function () {
             this.newEnemy();
-        }.bind(this), 2000);
+        }.bind(this), 1500);
         this.newEnemy();
         this.keepRefresh();
     }
