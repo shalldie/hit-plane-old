d9fc06fa9f8ecfbd0535ebee51c4d688148688c6
谢爽 <394229785@qq.com>
2017-05-29 04:52:00 +0800

    修改注释，正则

--- src/loader.ts
+++ src/loader.ts
@@ -1,9 +1,17 @@
+// 加载样式
 import './style.scss';
 
+// 预加载图片
 let imgSrcArr = ["bg.jpg", "boom.png", "bullet01.png", "bullet02.png", "bullet03.png", "enemy.png", "enemy_bullet.png", "heart.png", "hp.png", "plane.png"];
 
 let loadedImgCount: number = 0;
 
+/**
+ * 根据图片地址返回一个预加载Promise
+ * 
+ * @param {string} imgSrc 
+ * @returns {Promise<HTMLImageElement>} 
+ */
 function updateLoading(imgSrc: string): Promise<HTMLImageElement> {
     return new Promise<HTMLImageElement>(res => {
         let img = new Image();
@@ -19,8 +27,8 @@ function updateLoading(imgSrc: string): Promise<HTMLImageElement> {
 (async () => {
     let imgArr = await Promise.all(imgSrcArr.map(src => updateLoading('img/' + src)));
     let imgDict: object = {};
-    for (let i = 0, len = imgArr.length; i < len; i++) {  // for 要比 forEach,map 等效率高不少，，在v8下
-        let name = imgArr[i].src.replace(/^[^\/]*\/|\.[^\.]+$/g, '');
+    for (let i = 0, len = imgArr.length; i < len; i++) {  // for 要比 forEach,map 等效率高不少，，在V8下
+        let name = imgArr[i].src.replace(/^\S*\/|\.[^\.]*$/g, '');
         if (name != 'bg') imgDict[name] = imgArr[i];
     }
 })();
